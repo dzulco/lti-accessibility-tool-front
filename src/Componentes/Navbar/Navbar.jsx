20015fb9827815b07421b7af81ecf3fba2f067a3
dzulco <96262387+dzulco@users.noreply.github.com>
2026-07-24 00:31:34 -0300

    Update logo image source path in Navbar component

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
     <div className="logo-container">
         <img
-            src="../../../public/Nexa-Logo.png"
+            src="/Nexa-Logo.png" 
             className="logo"
             alt="Logo Nexa"
         />
